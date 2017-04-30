cdeafcf7cadcc3c2e93f6f52b6cb7aeba7966883
Erika Silva <erika.silva@itriad.org.br>
2017-04-30 14:37:20 -0400

    Criei método pra configurar fase

--- src/main/webapp/app/modules/admin/dashboard/dashboardCtrl.js
+++ src/main/webapp/app/modules/admin/dashboard/dashboardCtrl.js
@@ -13,37 +13,27 @@
 		.module('dashboard')
 		.controller('DashboardCtrl', Dashboard);
 
-	Dashboard.$inject = ['faseService', '$state', 'authService'];
+	Dashboard.$inject = ['faseService', '$state'];
 
-	function Dashboard(faseService, $state, authService) {
+	function Dashboard(faseService, $state) {
+
 		var context = this;
 		onOpenPage();
 
 		context.configuraFase = configuraFase;
 
 		function onOpenPage(){
-			/*faseService.getAll()
+			faseService.getAll()
 				.then(function (successResponse) {
 					context.fases = successResponse.data.list;
 				})
 				.catch(function (errorResponse) {
 					console.log("Error >>> ", errorResponse);
-				})*/
-            // listaFases();
+				})
 		}
 
-        function listaFases(){
-            authService.lista()
-                .then(function(success){
-                    console.info(success)
-                })
-                .catch(function (error) {
-                    console.error(error);
-                })
-        }
-
 		function configuraFase(fase){
-			$state.go('adminHome.fase', { fase: fase });
+			$state.go('authenticated.adminHome.fase', { fase: fase });
 		}
 	}
 })();
