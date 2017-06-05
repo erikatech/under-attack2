4c0cae36cf69f15731e313301e18440b3178f28a
Erika Silva <erika.silva@itriad.org.br>
2017-06-05 14:02:49 -0400

    Ajustei o controller para conter as operações da sala dos testadores

--- src/main/java/br/edu/ifam/underattack/controller/SalaTestadoresController.java
+++ src/main/java/br/edu/ifam/underattack/controller/SalaTestadoresController.java
@@ -1,14 +1,22 @@
 package br.edu.ifam.underattack.controller;
 
 import br.com.caelum.vraptor.*;
+import br.com.caelum.vraptor.validator.I18nMessage;
+import br.com.caelum.vraptor.validator.Validator;
 import br.com.caelum.vraptor.view.Results;
-import br.edu.ifam.underattack.dao.*;
+import br.edu.ifam.underattack.dao.AlunoDao;
+import br.edu.ifam.underattack.dao.DesafioDao;
+import br.edu.ifam.underattack.dao.ValorDeEntradaDao;
 import br.edu.ifam.underattack.model.*;
 import br.edu.ifam.underattack.model.enums.IndicadorFase;
 import br.edu.ifam.underattack.model.enums.SituacaoDesafio;
+import br.edu.ifam.underattack.model.enums.TipoValorEntrada;
 
 import javax.inject.Inject;
 import javax.persistence.NoResultException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by erika.silva on 04/06/2017.
@@ -22,16 +30,23 @@ public class SalaTestadoresController {
 
     private final DesafioDao desafioDao;
 
+    private final ValorDeEntradaDao valorDeEntradaDao;
+
+    private final Validator validator;
+
     @Inject
-    public SalaTestadoresController(Result result, AlunoDao alunoDao, DesafioDao desafioDao){
+    public SalaTestadoresController(Result result, AlunoDao alunoDao,
+                    DesafioDao desafioDao, ValorDeEntradaDao valorDeEntradaDao, Validator validator){
         this.result = result;
         this.alunoDao = alunoDao;
         this.desafioDao = desafioDao;
+        this.valorDeEntradaDao = valorDeEntradaDao;
+        this.validator = validator;
     }
 
     @Deprecated
     public SalaTestadoresController(){
-        this(null, null, null);
+        this(null, null, null, null, null);
     }
 
 
@@ -48,8 +63,18 @@ public class SalaTestadoresController {
     public void iniciaDesafio(Long idDesafio, String login){
         try {
             AlunoRealizaDesafio alunoRealizaDesafio = this.alunoDao.desafiosDoAluno(login, idDesafio);
+
+            boolean alunoEncontrouValores = this.alunoDao.alunoEncontrouValores(login);
+            validator.addIf(alunoEncontrouValores, new I18nMessage("aluno.valores","aluno.encontrou.valores"));
+            validator.onErrorSendBadRequest();
+
+            Collections.shuffle(alunoRealizaDesafio.getDesafio().getPrograma().getValoresEntrada());
+            if(alunoRealizaDesafio.getCerebrosDisponiveis() == 0){
+                alunoRealizaDesafio.setCerebrosDisponiveis(3);
+            }
             this.result.use(Results.json()).from(alunoRealizaDesafio, "alunoDesafio")
-                    .include("desafio", "desafio.programa", "desafio.programa.valoresEntrada").serialize();
+                    .include("desafio", "desafio.programa", "desafio.programa.valoresEntrada")
+                    .exclude("desafio.programa.valoresEntrada.tipo").serialize();
         } catch(NoResultException ex){
             Desafio desafio = this.desafioDao.getDesafio(idDesafio);
             Aluno aluno = this.alunoDao.consulta(login);
@@ -60,9 +85,53 @@ public class SalaTestadoresController {
             alunoDesafio.setIndicadorFase(IndicadorFase.FASE_TESTADORES);
             alunoDesafio.setSituacaoDesafio(SituacaoDesafio.EM_ANDAMENTO);
             this.alunoDao.iniciaDesafio(alunoDesafio);
+            Collections.shuffle(alunoDesafio.getDesafio().getPrograma().getValoresEntrada());
+
             this.result.use(Results.json()).from(alunoDesafio, "alunoDesafio")
                     .include("desafio", "desafio.programa", "desafio.programa.valoresEntrada").serialize();
         }
+    }
+
+    @Get
+    public void isValorValido(Long idValorDeEntrada, String login, Long desafioId){
+        ValorDeEntrada valorDeEntrada = this.valorDeEntradaDao.getValorDeEntrada(idValorDeEntrada);
+        if(valorDeEntrada.getTipo().equals(TipoValorEntrada.CORRETO)){
+            this.result.use(Results.http())
+                    .body(String.valueOf(valorDeEntrada.getDificuldade()
+                            .getPontos()));
+        } else {
+            AlunoRealizaDesafio alunoRealizaDesafio = this.alunoDao.desafiosDoAluno(login, desafioId);
+            alunoRealizaDesafio.setCerebrosDisponiveis(alunoRealizaDesafio.getCerebrosDisponiveis() - 1);
+            this.result.include("errorMessage", "Valor Inválido").use(Results.http()).sendError(500);
+        }
+    }
+
+    @Post
+    @Consumes(value = "application/json")
+    public void goToNextStage(String login, List<ValorDeEntrada> valoresSelecionados){
+        boolean hasAtLeastOne = this.hasAtLeastOne(valoresSelecionados);
+        validator.addIf(!hasAtLeastOne, new I18nMessage("valoresIncorretos","sem.valores.corretos"));
+        validator.onErrorSendBadRequest();
+        this.alunoDao.encontraValoresDeEntrada(login, valoresSelecionados);
+        this.result.use(Results.status()).ok();
+    }
+
+    @Get
+    public void getValoresAluno(String login){
+        List<AlunoEncontraValorDeEntrada> valoresAluno = this.valorDeEntradaDao.getValoresAluno(login);
+        this.result.use(Results.json()).from(valoresAluno, "valores")
+                .include("valorDeEntrada").exclude("valorDeEntrada.tipo").serialize();
+    }
+
+    private boolean hasAtLeastOne(List<ValorDeEntrada> valoresSelecionados) {
+        List<ValorDeEntrada> finalList = new ArrayList<>();
+        for (ValorDeEntrada valorSelecionado : valoresSelecionados) {
+            ValorDeEntrada valorDeEntrada = this.valorDeEntradaDao.getValorDeEntrada(valorSelecionado.getId());
+            if (valorDeEntrada.getTipo().equals(TipoValorEntrada.CORRETO)){
+                finalList.add(valorDeEntrada);
+            }
+        }
+        return finalList.size() != 0;
     }
 
 }
