e604cf3f7bfef671c8e0b2ef6d671cd069e856c4
Erika Silva <erika.silva@itriad.org.br>
2017-06-05 14:01:01 -0400

    Criei controller pra listar os desafios do aluno

--- src/main/java/br/edu/ifam/underattack/controller/DesafioController.java
+++ src/main/java/br/edu/ifam/underattack/controller/DesafioController.java
@@ -22,109 +22,37 @@ import java.util.List;
  * Created by erika.silva on 04/06/2017.
  */
 @Controller
-public class SalaTestadoresController {
+public class DesafioController {
 
     private final Result result;
 
     private final AlunoDao alunoDao;
 
-    private final DesafioDao desafioDao;
-
-    private final ValorDeEntradaDao valorDeEntradaDao;
-
     private final Validator validator;
 
     @Inject
-    public SalaTestadoresController(Result result, AlunoDao alunoDao,
-                    DesafioDao desafioDao, ValorDeEntradaDao valorDeEntradaDao, Validator validator){
+    public DesafioController(Result result, AlunoDao alunoDao, Validator validator){
         this.result = result;
         this.alunoDao = alunoDao;
-        this.desafioDao = desafioDao;
-        this.valorDeEntradaDao = valorDeEntradaDao;
         this.validator = validator;
+
     }
 
     @Deprecated
-    public SalaTestadoresController(){
-        this(null, null, null, null, null);
+    public DesafioController(){
+        this(null, null, null);
     }
 
 
     @Get
-    public void getPocaoMagicaItems(String login){
-        Aluno alunoConsultado = this.alunoDao.consulta(login);
-        PocaoMagica pocaoAluno = alunoConsultado.getPocaoMagica();
-        this.result.use(Results.json()).from(pocaoAluno, "ingredientes")
-                .include("pocaoIngredienteList").serialize();
-    }
-
-    @Post
-    @Consumes(value = "application/json")
-    public void iniciaDesafio(Long idDesafio, String login){
-        try {
-            AlunoRealizaDesafio alunoRealizaDesafio = this.alunoDao.desafiosDoAluno(login, idDesafio);
-
-            boolean alunoEncontrouValores = this.alunoDao.alunoEncontrouValores(login);
-            validator.addIf(alunoEncontrouValores, new I18nMessage("aluno.valores","aluno.encontrou.valores"));
-            validator.onErrorSendBadRequest();
-
-            Collections.shuffle(alunoRealizaDesafio.getDesafio().getPrograma().getValoresEntrada());
-            if(alunoRealizaDesafio.getCerebrosDisponiveis() == 0){
-                alunoRealizaDesafio.setCerebrosDisponiveis(3);
-            }
-            this.result.use(Results.json()).from(alunoRealizaDesafio, "alunoDesafio")
-                    .include("desafio", "desafio.programa", "desafio.programa.valoresEntrada")
-                    .exclude("desafio.programa.valoresEntrada.tipo").serialize();
-        } catch(NoResultException ex){
-            Desafio desafio = this.desafioDao.getDesafio(idDesafio);
-            Aluno aluno = this.alunoDao.consulta(login);
-            AlunoRealizaDesafio alunoDesafio = new AlunoRealizaDesafio();
-            alunoDesafio.setAluno(aluno);
-            alunoDesafio.setDesafio(desafio);
-            alunoDesafio.setCerebrosDisponiveis(3);
-            alunoDesafio.setIndicadorFase(IndicadorFase.FASE_TESTADORES);
-            alunoDesafio.setSituacaoDesafio(SituacaoDesafio.EM_ANDAMENTO);
-            this.alunoDao.iniciaDesafio(alunoDesafio);
-            Collections.shuffle(alunoDesafio.getDesafio().getPrograma().getValoresEntrada());
-
-            this.result.use(Results.json()).from(alunoDesafio, "alunoDesafio")
-                    .include("desafio", "desafio.programa", "desafio.programa.valoresEntrada").serialize();
-        }
-    }
-
-    @Get
-    public void isValorValido(Long idValorDeEntrada, String login, Long desafioId){
-        ValorDeEntrada valorDeEntrada = this.valorDeEntradaDao.getValorDeEntrada(idValorDeEntrada);
-        if(valorDeEntrada.getTipo().equals(TipoValorEntrada.CORRETO)){
-            this.result.use(Results.http())
-                    .body(String.valueOf(valorDeEntrada.getDificuldade()
-                            .getPontos()));
-        } else {
-            AlunoRealizaDesafio alunoRealizaDesafio = this.alunoDao.desafiosDoAluno(login, desafioId);
-            alunoRealizaDesafio.setCerebrosDisponiveis(alunoRealizaDesafio.getCerebrosDisponiveis() - 1);
-            this.result.include("errorMessage", "Valor Inválido").use(Results.http()).sendError(500);
-        }
-    }
-
-    @Post
-    @Consumes(value = "application/json")
-    public void goToNextStage(String login, List<ValorDeEntrada> valoresSelecionados){
-        boolean hasAtLeastOne = this.hasAtLeastOne(valoresSelecionados);
-        validator.addIf(!hasAtLeastOne, new I18nMessage("valoresIncorretos","sem.valores.corretos"));
+    public void getDesafiosAluno(String login, Long idDesafio){
+        boolean alunoEncontrouValores = this.alunoDao.alunoEncontrouValores(login);
+        validator.addIf(!alunoEncontrouValores, new I18nMessage("aluno.valores","aluno.encontrou.valores"));
         validator.onErrorSendBadRequest();
-        this.alunoDao.encontraValoresDeEntrada(login, valoresSelecionados);
-        this.result.use(Results.status()).ok();
-    }
 
-    private boolean hasAtLeastOne(List<ValorDeEntrada> valoresSelecionados) {
-        List<ValorDeEntrada> finalList = new ArrayList<>();
-        for (ValorDeEntrada valorSelecionado : valoresSelecionados) {
-            ValorDeEntrada valorDeEntrada = this.valorDeEntradaDao.getValorDeEntrada(valorSelecionado.getId());
-            if (valorDeEntrada.getTipo().equals(TipoValorEntrada.CORRETO)){
-                finalList.add(valorDeEntrada);
-            }
-        }
-        return finalList.size() != 0;
+        AlunoRealizaDesafio alunoRealizaDesafio = this.alunoDao.desafiosDoAluno(login, idDesafio);
+        this.result.use(Results.json()).from(alunoRealizaDesafio, "alunoDesafio")
+                .include("desafio", "desafio.programa", "desafio.programa.valoresEntrada")
+                .exclude("desafio.programa.valoresEntrada.tipo").serialize();
     }
-
 }
